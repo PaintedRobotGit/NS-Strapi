c44bfac720e137011dcd57bbb97ad8c3e86b4dad
GoOnGithub <ryan@paintedrobot.com>
2025-09-05 17:42:03 -0400

    revert

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,20 +2,6 @@ export default ({ env }) => ({
     'users-permissions': {
         config: {
             jwtSecret: env('JWT_SECRET'),
-            providers: {
-                google: {
-                    enabled: true,
-                    clientId: env('GOOGLE_CLIENT_ID'),
-                    clientSecret: env('GOOGLE_CLIENT_SECRET'),
-                    scope: [
-                        'openid',
-                        'email',
-                        'profile',
-                        'https://www.googleapis.com/auth/userinfo.profile',
-                        'https://www.googleapis.com/auth/userinfo.email'
-                    ],
-                },
-            },
         },
     },
     upload: {
