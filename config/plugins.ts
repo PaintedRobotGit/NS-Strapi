b7df1fff2ef0b83b895e3196e3cfcf98f20e4033
GoOnGithub <ryan@paintedrobot.com>
2025-09-05 17:37:15 -0400

    scope update for google auth

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,6 +2,20 @@ export default ({ env }) => ({
     'users-permissions': {
         config: {
             jwtSecret: env('JWT_SECRET'),
+            providers: {
+                google: {
+                    enabled: true,
+                    clientId: env('GOOGLE_CLIENT_ID'),
+                    clientSecret: env('GOOGLE_CLIENT_SECRET'),
+                    scope: [
+                        'openid',
+                        'email',
+                        'profile',
+                        'https://www.googleapis.com/auth/userinfo.profile',
+                        'https://www.googleapis.com/auth/userinfo.email'
+                    ],
+                },
+            },
         },
     },
     upload: {
